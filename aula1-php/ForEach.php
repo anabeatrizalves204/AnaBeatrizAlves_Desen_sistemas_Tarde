9a234933f7d035c73cc125be6bb878265a9fbc37
anabeatrizalves204 <alvesanabeatriz204@gmail.com>
2025-04-16 16:04:21 -0300

    For Each php
    exemplo php

---
+++ aula1-php/ForEach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $cores = array("Amarelo", "Vermelho", "Verde", "Azul");
+        foreach($cores as $cor)
+        {
+            echo $cor."<br/>";
+        }
+    ?>
+
+<h1>Aluna: Ana Beatriz Alves</h1>
+</body>
+</html>
